b414fe1c32ea9853b926d99ebadb8634707fa83d
TheAiro <theairo123321@gmail.com>
2023-08-05 13:05:05 +0300

    Add initial project files and configuration

---
+++ app/page.js
@@ -0,0 +1,91 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export default function Home() {
+    const [seconds, setSeconds] = useState(1800)
+    const [stop, setStop] = useState(false)
+    const [flaw, setFlaw] = useState(0)
+    const [settings, setSettings] =useState(true);
+
+    const [inputValue1, setInputValue1] = useState(0);
+    const [inputValue2, setInputValue2] = useState(0);
+    const [inputValue3, setInputValue3] = useState(0);
+    const [inputValue4, setInputValue4] = useState(0);
+    
+    useEffect(() => {
+        if (seconds <= 0 || stop) {
+          return;
+        }
+        let interval ;
+        if (flaw>0) {
+            interval = setInterval(() => {
+                setFlaw(current => {
+                  if (current <= 1) {
+                    clearInterval(interval); // Clear the interval when seconds reach 0
+                    return 0; // Set seconds to 0
+                  } else {
+                    return current - 1; // Decrement seconds by 1
+                  }
+                });
+              }, 1000);
+        }
+        else {
+            interval = setInterval(() => {
+                setSeconds(current => {
+                  if (current <= 1) {
+                    clearInterval(interval); // Clear the interval when seconds reach 0
+                    return 0; // Set seconds to 0
+                  } else {
+                    return current - 1; // Decrement seconds by 1
+                  }
+                });
+              }, 1000);
+        }
+        
+      
+        return () => clearInterval(interval);
+      }, [seconds, stop, flaw]);
+
+    function restart() {
+        setSettings(true)
+        setFlaw(0)
+        setStop(false)
+    }
+
+    function newCycle() {
+        setSettings(false)
+        setSeconds(parseInt(inputValue1) * 600 +
+        parseInt(inputValue2) * 60 +
+        parseInt(inputValue3) * 10 +
+        parseInt(inputValue4))
+    }
+
+    return (
+        <div className='flex items-center flex-col'>
+            <div className='font-medium text-4xl mt-16 mb-10'>Mindfulness Clock</div>
+            <div className={`font-medium text-8xl mb-6 ${settings && 'hidden'} ${!stop && !flaw ? 'text-black' : 'text-gray-600'}`}>{Math.floor(seconds/60)%60<10 ? `0${Math.floor(seconds/60) % 60}` : Math.floor(seconds/60)%60}:{seconds%60<10 ? `0${seconds % 60}` : seconds%60}</div>
+            <div className={`flex items-center font-medium text-8xl mb-6  ${!settings && 'hidden'}`}>
+                <input type="number" onInput={(e) => setInputValue1(Math.max(Math.min(5,e.target.value),0))} value={inputValue1} className="appearance-none w-20 border-2 mr-2 rounded-xl"/>
+                <input type="number" onInput={(e) => setInputValue2(Math.max(Math.min(5,e.target.value),0))} value={inputValue2} className="w-20 border-2 mr-4 rounded-xl"/>
+                : 
+                <input type="number" onInput={(e) => setInputValue3(Math.max(Math.min(5,e.target.value),0))} value={inputValue3} className="w-20 border-2 ml-4 mr-2 rounded-xl"/>
+                <input type="number" onInput={(e) => setInputValue4(Math.max(Math.min(5,e.target.value),0))} value={inputValue4} className="w-20 border-2 mr-2 rounded-xl"/>
+            </div>
+            <button onClick={newCycle}className={`text-lg font-medium  w-24 h-24 rounded-full bg-lime-500 cursor-pointer hover:bg-lime-600 active:bg-lime-700 ${!settings && 'hidden'}`}>Confirm</button>
+            <div className={`font-medium text-xl mb-16 text-gray-600  ${!stop && 'opacity-0'}`}>The clock is stopped</div>
+            <div className='flex items-end text-lg font-medium'>
+                <div className="flex flex-col">
+                <div className={`text-center mb-1 ${!flaw && 'opacity-0'}`}>00:{flaw%60<10 ? `0${flaw % 60}` : flaw%60}</div>
+                    <button onClick={() => setFlaw(current => 30)}className={`w-24 h-24 rounded-full bg-red-500  cursor-pointer hover:bg-red-600 active:bg-red-700 ${settings && 'opacity-0'}`}>Flaw</button>
+                </div>
+                <button onClick={() => setStop(current => !current)}className={`ml-8 w-24 h-24 rounded-full bg-blue-500 cursor-pointer hover:bg-blue-600 active:bg-blue-700 ${settings && 'opacity-0'}`}>Stop</button>
+            </div>
+            <div className="flex justify-center">
+                <button onClick={restart}className={`text-lg font-medium  w-24 h-24 rounded-full bg-green-500 cursor-pointer hover:bg-green-600 active:bg-green-700 ${!stop && 'opacity-0'}`}>Restart</button>
+            </div>
+            
+            
+        </div>
+    )
+}
